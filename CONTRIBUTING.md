66db63c8600f243ca474714a51fc65de4f4730e1
alainbodiguel <alain.bodiguel@gisaia.com>
2020-11-23 20:48:59 +0100

    Stub

---
+++ CONTRIBUTING.md
@@ -0,0 +1,48 @@
+# Contributing to ARLAS-permissions
+
+ARLAS-permissions is an open source project and there are many ways to contribute.
+
+## Bug reports
+
+If you think you have found a bug in ARLAS-permissions, first make sure that it has not been already addressed in our
+[issues list](https://github.com/gisaia/ARLAS-permissions/issues).
+
+If not, provide as much information as you can to help us reproduce your bug :
+
+- ARLAS-permissions version and configuration file used
+- requests and data payloads to reproduce the bug
+
+Keep in mind that we will fix your problem faster if we can easily reproduce it.
+
+## Feature requests
+
+If you think ARLAS-permissions lacks a feature, do not hesitate to open an issue on our
+[issues list](https://github.com/gisaia/ARLAS-permissions/issues) on GitHub which describes what you need, why you need it,
+and how it should work.
+
+## Contributing code and documentation changes
+
+If you want to submit a bugfix or a feature implementation, first find or open an issue about it on our
+[issues list](https://github.com/gisaia/ARLAS-permissions/issues)
+
+#### Prerequisites
+
+ARLAS-permissions runs with JDK8 and is built/packaged with maven 3.
+Codebase follows [IntelliJ](https://www.jetbrains.com/idea/) default formatting rules.
+
+#### Fork and clone the repository
+
+You will need to fork the main ARLAS-permissions repository and clone it to your local machine. See
+[github help page](https://help.github.com/articles/fork-a-repo) for help.
+
+#### Submitting your changes
+
+When your code is ready, you will have to :
+
+- rebase your repository.
+- run ./scripts/tests-integration.sh which should exit with a `0` status.
+- update documentation in `docs/` and tests in `src/test` if relevant
+- [submit a pull request](https://help.github.com/articles/using-pull-requests) with a proper title and a mention to
+the corresponding issue (eg "fix #1234").
+- never force push your branch after submitting, if you need to sync with official repository, you should better merge
+master into your branch.
---
+++ arlas-permissions-server/src/main/java/io/arlas/permissions/server/app/ArlasPermissionsServer.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to Gisaïa under one or more contributor
+ * license agreements. See the NOTICE.txt file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Gisaïa licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.arlas.permissions.server.app;
+
+import brave.http.HttpTracing;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.smoketurner.dropwizard.zipkin.ZipkinBundle;
+import com.smoketurner.dropwizard.zipkin.ZipkinFactory;
+import io.arlas.permissions.rest.PermissionsRestService;
+import io.arlas.server.app.ArlasCorsConfiguration;
+import io.arlas.server.auth.AuthenticationFilter;
+import io.arlas.server.auth.AuthorizationFilter;
+import io.arlas.server.exceptions.ArlasExceptionMapper;
+import io.arlas.server.exceptions.ConstraintViolationExceptionMapper;
+import io.arlas.server.exceptions.IllegalArgumentExceptionMapper;
+import io.arlas.server.utils.InsensitiveCaseFilter;
+import io.arlas.server.utils.PrettyPrintFilter;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.jersey.jackson.JsonProcessingExceptionMapper;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.ws.rs.core.HttpHeaders;
+import java.util.EnumSet;
+import java.util.Optional;
+
+public class ArlasPermissionsServer extends Application<io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration> {
+    Logger LOGGER = LoggerFactory.getLogger(ArlasPermissionsServer.class);
+
+    public static void main(String... args) throws Exception {
+        new ArlasPermissionsServer().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration> bootstrap) {
+        bootstrap.registerMetrics();
+        bootstrap.setConfigurationSourceProvider(new SubstitutingSourceProvider(
+                bootstrap.getConfigurationSourceProvider(), new EnvironmentVariableSubstitutor(false)));
+        bootstrap.addBundle(new SwaggerBundle<io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration>() {
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration configuration) {
+                return configuration.swaggerBundleConfiguration;
+            }
+        });
+        bootstrap.addBundle(new ZipkinBundle<io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration>(getName()) {
+            @Override
+            public ZipkinFactory getZipkinFactory(io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration configuration) {
+                return configuration.zipkinConfiguration;
+            }
+        });
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/", "index.html"));
+    }
+
+    @Override
+    public void run(io.arlas.permissions.server.app.ArlasPermissionsServerConfiguration configuration, Environment environment) throws Exception {
+
+        configuration.check();
+        LOGGER.info("Checked configuration: " + (new ObjectMapper()).writer().writeValueAsString(configuration));
+
+        if (configuration.zipkinConfiguration != null) {
+            Optional<HttpTracing> tracing = configuration.zipkinConfiguration.build(environment);
+        }
+
+        environment.getObjectMapper().setSerializationInclusion(Include.NON_NULL);
+        environment.getObjectMapper().configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, false);
+        environment.jersey().register(MultiPartFeature.class);
+        environment.jersey().register(new ArlasExceptionMapper());
+        environment.jersey().register(new IllegalArgumentExceptionMapper());
+        environment.jersey().register(new JsonProcessingExceptionMapper());
+        environment.jersey().register(new ConstraintViolationExceptionMapper());
+
+        environment.jersey().register(new PermissionsRestService());
+
+        // Auth
+        if (configuration.arlasAuthConfiguration.enabled) {
+            environment.jersey().register(new AuthenticationFilter(configuration.arlasAuthConfiguration));
+            environment.jersey().register(new AuthorizationFilter(configuration.arlasAuthConfiguration));
+        }
+
+        //cors
+        if (configuration.arlarsCorsConfiguration.enabled) {
+            configureCors(environment, configuration.arlarsCorsConfiguration);
+        } else {
+            CrossOriginFilter filter = new CrossOriginFilter();
+            final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CrossOriginFilter", filter);
+            // Expose always HttpHeaders.WWW_AUTHENTICATE to authentify on client side a non public uri call
+            cors.setInitParameter(CrossOriginFilter.EXPOSED_HEADERS_PARAM, HttpHeaders.WWW_AUTHENTICATE);
+        }
+
+        //filters
+        environment.jersey().register(PrettyPrintFilter.class);
+        environment.jersey().register(InsensitiveCaseFilter.class);
+    }
+
+    private void configureCors(Environment environment, ArlasCorsConfiguration configuration) {
+        CrossOriginFilter filter = new CrossOriginFilter();
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CrossOriginFilter", filter);
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, configuration.allowedOrigins);
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, configuration.allowedHeaders);
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, configuration.allowedMethods);
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, String.valueOf(configuration.allowedCredentials));
+        String exposedHeader = configuration.exposedHeaders;
+        // Expose always HttpHeaders.WWW_AUTHENTICATE to authentify on client side a non public uri call
+        if(configuration.exposedHeaders.indexOf(HttpHeaders.WWW_AUTHENTICATE)<0){
+            exposedHeader = configuration.exposedHeaders.concat(",").concat(HttpHeaders.WWW_AUTHENTICATE);
+        }
+        cors.setInitParameter(CrossOriginFilter.EXPOSED_HEADERS_PARAM, exposedHeader);
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+    }
+}
